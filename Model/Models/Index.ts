b7e30549eb6956665378b74108fa43e47a4da432
Simon Larsen <simonlarsen@oneuptime.com>
2023-08-21 17:23:19 +0100

    add clickhouse service

--- Model/Models/Index.ts
+++ Model/Models/Index.ts
@@ -117,6 +117,10 @@ import IncidentTemplate from './IncidentTemplate';
 import IncidentTemplateOwnerTeam from './IncidentTemplateOwnerTeam';
 import IncidentTemplateOwnerUser from './IncidentTemplateOwnerUser';
 
+import AnalyticsBaseModel from 'Common/Models/AnalyticsBaseModel';
+
+import Log from './Log';
+
 export default [
     User,
     Probe,
@@ -217,4 +221,9 @@ export default [
     IncidentTemplate,
     IncidentTemplateOwnerTeam,
     IncidentTemplateOwnerUser,
+];
+
+
+export const AnalyticsModels: Array<typeof AnalyticsBaseModel> = [
+    Log
 ];
