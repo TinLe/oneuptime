bb283c188454fd372962373d3d531e1ea9ed33bc
Tin Le <tinvle@gmail.com>
2023-09-11 21:26:10 +0200

    Notes and helper scripts

---
+++ stop-oneuptime.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ONEUPTIME_HOME=/home/oneuptime
+
+# make sure we are in correct directory
+cd ${ONEUPTIME_HOME}
+
+export $(grep -v '^#' config.env | xargs) && docker compose down
